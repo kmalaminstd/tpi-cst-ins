f561dd2bc5a7450229e801204b2d8c195bb9d6d2
kmalamin <alaminkhanstd@gmail.com>
2023-03-04 13:48:48 +0600

    first version

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const tabBtn = document.querySelectorAll('.home-tab .home-tab-nav ul li')
+const tabCnt = document.querySelectorAll('.home-tab-content .achievement-content')
+
+for(let i = 0; i < tabBtn.length; i++){
+    tabBtn[i].addEventListener('click', () => {
+
+        
+        for(let k = 0; k < tabBtn.length; k++){
+            tabBtn[k].classList.remove('active')
+        }
+        tabBtn[i].classList.add('active')
+
+        for(let j = 0; j < tabCnt.length; j++){
+            tabCnt[j].classList.remove('achievement-content-active')
+        }
+        tabCnt[i].classList.add('achievement-content-active')
+
+        
+    })
+}
